07923f0f88fc5dc59b18751e1fce3003177a1cfa
AkashSDas <aakashdas368@gmail.com>
2022-01-09 18:25:38 +0530

    add morgan package

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,12 +1,14 @@
 import cors from "cors";
 import express from "express";
+import * as morgan from "morgan";
 
 // App
 export const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json()); // parse incoming data with Content-Type as application/json
+app.use(express.json());
+app.use(morgan.default("tiny"));
 
 // Test api route
-// app.get("/", (_, res) => res.status(200).send("Hello mom"));
+app.get("/", (_, res) => res.status(200).send("Hello mom"));
