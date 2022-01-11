81a36db36da15b8fb5630f29b22c38247d909d27
AkashSDas <aakashdas368@gmail.com>
2022-01-11 10:26:39 +0530

    update app.listen method's callback

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,5 +7,5 @@ import { app } from "./api";
 // Start the server
 const port = process.env.PORT || 8000;
 app.listen(port, () =>
-  console.log(`API is available on http://localhost:${port}`)
+  console.log(`API is available on http://localhost:${port}/api`)
 );
