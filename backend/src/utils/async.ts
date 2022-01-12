bad183470c7da8473baae59f9695351c0f7385e0
AkashSDas <aakashdas368@gmail.com>
2022-01-12 11:00:34 +0530

    create run async util

---
+++ backend/src/utils/async.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+import { AsnycMiddleware } from "./types";
+
+// Catching Errors in Async Functions
+export const runAsync = (fn: AsnycMiddleware) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    return fn(req, res, next);
+  };
+};
