659a9586777bbab957d800e73fc4641595c251b5
AkashSDas <aakashdas368@gmail.com>
2022-01-12 17:09:10 +0530

    chain catch to handle error

--- backend/src/utils/async.ts
+++ backend/src/utils/async.ts
@@ -4,6 +4,6 @@ import { AsnycMiddleware } from "./types";
 // Catching Errors in Async Functions
 export const runAsync = (fn: AsnycMiddleware) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    return fn(req, res, next);
+    return fn(req, res, next).catch(next);
   };
 };
