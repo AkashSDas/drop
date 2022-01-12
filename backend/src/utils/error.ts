9f77704c82eba96859512a1b6d7fe515e55318d8
AkashSDas <aakashdas368@gmail.com>
2022-01-12 17:45:44 +0530

    create error handler

--- backend/src/utils/error.ts
+++ backend/src/utils/error.ts
@@ -1,3 +1,6 @@
+import { NextFunction, Request, Response } from "express";
+import { responseMsg } from "./response";
+
 export class BaseApiError extends Error {
   public msg: string;
   public statusCode: number;
@@ -13,3 +16,22 @@ export class BaseApiError extends Error {
     Error.captureStackTrace(this, this.constructor);
   }
 }
+
+export const errorHandler = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof BaseApiError) {
+    return responseMsg(res, {
+      statusCode: err.statusCode,
+      msg: err.msg,
+      isError: true,
+    });
+  }
+
+  const statusCode = (err as any)?.statusCode || 500;
+  const msg = (err as any)?.msg || "Something went wrong, Please try again";
+  return responseMsg(res, { statusCode, msg, isError: true });
+};
