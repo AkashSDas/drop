cfdb23e6207df9a8915b89554cd8cb24f2c40299
AkashSDas <aakashdas368@gmail.com>
2022-01-12 10:35:07 +0530

    create controller and middleware types

--- backend/src/utils/types.ts
+++ backend/src/utils/types.ts
@@ -1,25 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 
-/**
- * Base middleware type
- */
-export type Middleware = (
+export type AsnycMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => Promise<void>;
 
-/**
- * Middlewares type for middlewares where id will be only param
- */
-export type IdMiddleware = (
+export type AsnycIdMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction,
   id: string
 ) => Promise<void>;
 
-/**
- * Base async controller type
- */
 export type AsyncController = (req: Request, res: Response) => Promise<void>;
