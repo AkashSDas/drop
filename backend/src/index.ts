bbc844415ac75b051791357883197315fdbc60dc
AkashSDas <aakashdas368@gmail.com>
2022-01-13 20:38:50 +0530

    initialize cloudinary

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,13 @@ config();
 import { connectMongoDB } from "./config/db";
 connectMongoDB();
 
+import cloudinary from "cloudinary";
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
 import { app } from "./api";
 
 // Start the server
