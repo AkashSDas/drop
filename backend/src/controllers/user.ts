896c449c450d2c4b80a47dd8dcf8c3730bfd9487
AkashSDas <aakashdas368@gmail.com>
2022-01-13 08:15:09 +0530

    create controller and route for logout

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,5 +1,6 @@
 import { User } from "../models/user";
 import { BaseApiError } from "../utils/error";
+import { responseMsg } from "../utils/response";
 import { AsnycMiddleware } from "../utils/types";
 import { loginUser } from "../utils/user";
 
@@ -29,4 +30,16 @@ export const login: AsnycMiddleware = async (req, res, next) => {
 
   user.password = undefined; // no sending user password to client
   return loginUser(user, res, "Successfully logged in");
+};
+
+export const logout: AsnycMiddleware = async (req, res) => {
+  // removing token from cookie (although its not used for auth)
+  // since token is used for auth, its work in frontend to rm auth
+  // token from frontend
+  res.cookie("token", null, { expires: new Date(Date.now()), httpOnly: true });
+  return responseMsg(res, {
+    statusCode: 200,
+    isError: false,
+    msg: "Successfully logged out",
+  });
 };
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, signup } from "../controllers/user";
+import { login, logout, signup } from "../controllers/user";
 import { runAsync } from "../utils/async";
 import { errorHandler } from "../utils/error";
 
@@ -7,3 +7,4 @@ export const router = Router();
 
 router.post("/signup", runAsync(signup), errorHandler);
 router.post("/login", runAsync(login), errorHandler);
+router.get("/logout", logout);
