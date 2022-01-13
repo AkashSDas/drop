cc19a358b1e413e1dfe9b092808cef86e68ea5db
AkashSDas <aakashdas368@gmail.com>
2022-01-14 00:16:38 +0530

    create check roles middleware

--- backend/src/middlewares/user.ts
+++ backend/src/middlewares/user.ts
@@ -2,6 +2,7 @@ import { AsyncMiddleware } from "../utils/types";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user";
 import { BaseApiError } from "../utils/error";
+import { NextFunction, Request, Response } from "express";
 
 export const isLoggedIn: AsyncMiddleware = async (req, res, next) => {
   const token =
@@ -22,4 +23,23 @@ export const isLoggedIn: AsyncMiddleware = async (req, res, next) => {
 
   req.user = await User.findById(decoded.id);
   next();
+};
+
+// Here roles is an array of one role only. Using list here for ease of
+// working with array as opposed to string
+// check if roles (which is only one role) is equal to user's role
+export const checkRole = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      return next(new BaseApiError(401, "You are not logged in"));
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new BaseApiError(403, "You are not allowed for this resource")
+      );
+    }
+
+    next();
+  };
 };
