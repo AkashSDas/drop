72c56a745191b0b4fb2faa50f53dcf8a03c9c3fb
AkashSDas <aakashdas368@gmail.com>
2022-01-14 18:28:59 +0530

    check if redrop exists while creating redrop

--- backend/src/controllers/redrop.ts
+++ backend/src/controllers/redrop.ts
@@ -8,6 +8,11 @@ export const createReDrop: AsyncMiddleware = async (req, res, next) => {
   if (!req.user) return next(new BaseApiError(401, "You're not logged in"));
   const drop = await Drop.findById(req.params.dropId);
   if (!drop) return next(new BaseApiError(400, "Drop doesn't exists"));
+
+  // Check if redrop (for this drop and req.user) exists or not
+  const exists = await ReDrop.findOne({ drop: drop._id, user: req.user._id });
+  if (exists) return next(new BaseApiError(400, "Redrop already exists"));
+
   const reDrop = await ReDrop.create({ user: req.user._id, drop: drop._id });
   return responseMsg(res, {
     statusCode: 200,
