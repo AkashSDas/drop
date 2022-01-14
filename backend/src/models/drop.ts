966201c0a25a2edb45588a2d167473d5be2296cb
AkashSDas <aakashdas368@gmail.com>
2022-01-14 12:38:50 +0530

    create drop model

---
+++ backend/src/models/drop.ts
@@ -0,0 +1,58 @@
+import { Document, model, Model, Schema, SchemaTypes, Types } from "mongoose";
+import MongoPaging from "mongo-cursor-pagination";
+
+export interface IDropDocument extends Document {
+  user: Types.ObjectId;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export interface IDrop extends IDropDocument {}
+export interface IDropModel extends Model<IDrop> {}
+
+const dropSchema = new Schema<IDrop, IDropModel>(
+  {
+    user: {
+      type: SchemaTypes.ObjectId,
+      required: [true, "Author is required"],
+      ref: "User",
+    },
+    content: {
+      type: SchemaTypes.String,
+      required: [true, "Drop content is required"],
+      maxlength: [2048, "Drop content is above 2048 chars, its {VALUE}"],
+      minlength: [6, "Drop content is too short, it should be atleast 6 chars"],
+      trim: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// User can modify content only before 10mins and not after that
+dropSchema.pre("updateOne", async function (this: IDrop, next) {
+  const updateAllowedTill = new Date(
+    new Date(this.createdAt).getTime() + 10 * 60 * 1000
+  );
+
+  if (this.isModified("content") && updateAllowedTill.getTime() > Date.now()) {
+    return next();
+  }
+
+  throw Error(
+    "It's already above 10mins since the drop was created, it can't be modified now"
+  );
+});
+
+// Duplicate the ID field.
+dropSchema.virtual("id").get(function (this: IDrop) {
+  return this._id.toHexString();
+});
+
+// Ensure virtual fields are serialised.
+dropSchema.set("toJSON", { virtuals: true });
+
+// Pagination
+dropSchema.plugin(MongoPaging.mongoosePlugin, { name: "paginateDrop" });
+
+export const Drop = model<IDrop, IDropModel>("Drop", dropSchema);
