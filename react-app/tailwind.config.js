015161c942a1ba63f43c7f3e9b18ec0e90c21a7b
AkashSDas <aakashdas368@gmail.com>
2022-01-20 18:27:21 +0530

    setup tailwind css

---
+++ react-app/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      primary: "#202028",
+      secondary: "#3A8CFF",
+      text1: "#FFFEFF",
+      text2: "#9E9FA7",
+      icon: "#686875",
+      success: "#80B97C",
+      error: "#E15555",
+      card: "#252733",
+    },
+    fontFamily: {},
+  },
+  plugins: [],
+};
