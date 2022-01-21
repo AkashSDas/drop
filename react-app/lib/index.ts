cd3b105d5999daa85039073e41265e0847312e9b
AkashSDas <aakashdas368@gmail.com>
2022-01-21 18:57:53 +0530

    create run async helper

--- react-app/lib/index.ts
+++ react-app/lib/index.ts
@@ -12,3 +12,16 @@ export const monthNames = [
   "Nov",
   "Dec",
 ];
+
+interface IRunAsync {
+  result: any;
+  error: any;
+}
+export const runAsync = async (promise: Promise<any>): Promise<IRunAsync> => {
+  try {
+    const result = await promise;
+    return { result: result, error: null };
+  } catch (err) {
+    return { result: null, error: err };
+  }
+};
