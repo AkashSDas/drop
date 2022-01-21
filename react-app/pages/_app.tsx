4c224bfe2af19d3edb281cb3360c733d8b59af0a
AkashSDas <aakashdas368@gmail.com>
2022-01-21 10:21:35 +0530

    use header in _app.tsx

--- react-app/pages/_app.tsx
+++ react-app/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { IconlyProvider } from "react-iconly";
+import Header from "../components/shared/Header";
 import Sidebar from "../components/shared/Sidebar";
 import "../styles/main.scss";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <IconlyProvider set="light" primaryColor="#686875" size="medium">
+      <Header />
       <Sidebar />
       <Component {...pageProps} />
     </IconlyProvider>
