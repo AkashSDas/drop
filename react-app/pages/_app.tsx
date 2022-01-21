768b905dfe18a70ee2ecfc96cfd84b64475e81b7
AkashSDas <aakashdas368@gmail.com>
2022-01-21 13:08:09 +0530

    wrap app with user context

--- react-app/pages/_app.tsx
+++ react-app/pages/_app.tsx
@@ -1,15 +1,22 @@
+import { useReducer } from "react";
 import { IconlyProvider } from "react-iconly";
 import Header from "../components/shared/Header";
 import Sidebar from "../components/shared/Sidebar";
+import { UserContext, userInitialState } from "../lib/context/user";
+import userReducer from "../lib/context/user/reducer";
 import "../styles/main.scss";
 
 const MyApp = ({ Component, pageProps }) => {
+  const [user, dispatch] = useReducer(userReducer, userInitialState);
+
   return (
-    <IconlyProvider set="light" primaryColor="#686875" size="medium">
-      <Header />
-      <Sidebar />
-      <Component {...pageProps} />
-    </IconlyProvider>
+    <UserContext.Provider value={{ user, dispatch }}>
+      <IconlyProvider set="light" primaryColor="#686875" size="medium">
+        <Header />
+        <Sidebar />
+        <Component {...pageProps} />
+      </IconlyProvider>
+    </UserContext.Provider>
   );
 };
 
