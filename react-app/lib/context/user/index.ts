30984875e15c2e72889838e1ccca9660aaaeabec
AkashSDas <aakashdas368@gmail.com>
2022-01-21 18:58:05 +0530

    update user state interface shape

--- react-app/lib/context/user/index.ts
+++ react-app/lib/context/user/index.ts
@@ -1,20 +1,26 @@
 export interface IUserState {
-  id: string;
-  username: string;
-  email: string;
-  profilePicURL: {
+  token: string;
+  user: {
     id: string;
-    URL: string;
+    username: string;
+    email: string;
+    profilePicURL: {
+      id: string;
+      URL: string;
+    };
+    active: boolean;
+    role: "member" | "elder" | "co-leader" | "leader";
   };
-  active: boolean;
-  role: "member" | "elder" | "co-leader" | "leader";
 }
 
 export const userInitialState = {
-  id: null,
-  username: null,
-  email: null,
-  profilePicURL: null,
-  active: null,
-  role: null,
+  token: null,
+  user: {
+    id: null,
+    username: null,
+    email: null,
+    profilePicURL: null,
+    active: null,
+    role: null,
+  },
 };
