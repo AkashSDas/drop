68800c3bafe742f71a515d2835f3f1bd2e9c32d3
AkashSDas <aakashdas368@gmail.com>
2022-01-22 08:00:32 +0530

    create confirm password reset handler

--- react-app/lib/api/forgot-password.ts
+++ react-app/lib/api/forgot-password.ts
@@ -1,3 +1,4 @@
+import { IConfirmResetPasswordFormData } from "@context/confirm-reset-password";
 import { IForgotPasswordFormData } from "@context/forgot-password";
 import { axiosBaseInstance } from ".";
 import { runAsync } from "..";
@@ -9,6 +10,22 @@ export const forgotPasswordHandler = async (data: IForgotPasswordFormData) => {
     api(`/user/reset-password`, {
       method: "POST",
       data,
+      headers: { "Content-Type": "application/json" },
+    })
+  );
+
+  return response;
+};
+
+export const confirmResetPasswordHandler = async (
+  data: IConfirmResetPasswordFormData
+) => {
+  const api = axiosBaseInstance();
+
+  const response = await runAsync(
+    api(data.url, {
+      method: "POST",
+      data: { password: data.password },
       headers: { "Content-Type": "application/json" },
     })
   );
