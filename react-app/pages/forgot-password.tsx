4e121f0c8d4479f9af98a9f13da7eae634acda70
AkashSDas <aakashdas368@gmail.com>
2022-01-22 07:44:59 +0530

    add forgot password provider and form in forgot password page

--- react-app/pages/forgot-password.tsx
+++ react-app/pages/forgot-password.tsx
@@ -1,8 +1,22 @@
+import { useReducer } from "react";
+import forgotPasswordReducer from "@context/forgot-password/reducer";
+import { forgotPasswordInitialState } from "@context/forgot-password";
+import { ForgotPasswordContext } from "@context/forgot-password/context";
+import ForgotPasswordForm from "@components/forgot-password/ForgotPasswordForm";
+
 const ForgotPasswordPage = () => {
+  const [forgotPassword, dispatch] = useReducer(
+    forgotPasswordReducer,
+    forgotPasswordInitialState
+  );
+
   return (
-    <main>
-      <h3>Reset Password</h3>
-    </main>
+    <ForgotPasswordContext.Provider value={{ forgotPassword, dispatch }}>
+      <main className="space-y-8">
+        <h3>🤝 Login</h3>
+        <ForgotPasswordForm />
+      </main>
+    </ForgotPasswordContext.Provider>
   );
 };
 
