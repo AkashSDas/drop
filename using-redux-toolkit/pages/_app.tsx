c8fc62f38dbf04b374cf7bc3631435af2419ef58
AkashSDas <aakashdas368@gmail.com>
2022-01-23 10:13:57 +0530

    wrap app with iconly provider

--- using-redux-toolkit/pages/_app.tsx
+++ using-redux-toolkit/pages/_app.tsx
@@ -1,7 +1,12 @@
+import { IconlyProvider } from "react-iconly";
 import "../styles/main.scss";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <IconlyProvider set="light" primaryColor="#686875" size="medium">
+      <Component {...pageProps} />
+    </IconlyProvider>
+  );
 };
 
 export default MyApp;
