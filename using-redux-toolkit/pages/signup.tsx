79f1f2af39cae660286b066dadb6fb8379737609
AkashSDas <aakashdas368@gmail.com>
2022-01-23 16:35:42 +0530

    create signup page

---
+++ using-redux-toolkit/pages/signup.tsx
@@ -0,0 +1,72 @@
+import IconInput from "@components/shared/IconInput";
+import { Form, Formik } from "formik";
+import { useAppDispatch, useAppSelector } from "hooks/store";
+import { useRouter } from "next/router";
+import { Lock, Message, User } from "react-iconly";
+import { signupUser } from "store/signup/slice";
+
+const SignupPage = () => {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const loading = useAppSelector((state) => state.signup.loading);
+
+  const handleSubmit = async (value) => {
+    const isSignedIn = (await dispatch(signupUser(value))).payload;
+    if (isSignedIn) router.push("/login");
+  };
+
+  return (
+    <main>
+      <h3>🤝 Signup</h3>
+
+      <Formik
+        initialValues={{ username: "", email: "", password: "" }}
+        onSubmit={handleSubmit}
+      >
+        {({ values, handleChange, isSubmitting }) => (
+          <Form className="space-y-6">
+            <IconInput
+              icon={<User />}
+              inputProps={{
+                type: "text",
+                placeholder: "John Smith",
+                name: "username",
+                value: values.username,
+                onChange: handleChange,
+              }}
+            />
+            <IconInput
+              icon={<Message />}
+              inputProps={{
+                type: "email",
+                placeholder: "john@example.com",
+                name: "email",
+                value: values.email,
+                onChange: handleChange,
+              }}
+            />
+            <IconInput
+              icon={<Lock />}
+              inputProps={{
+                type: "password",
+                placeholder: "💧💧💧💧💧💧💧💧💧💧💧💧",
+                name: "password",
+                value: values.password,
+                onChange: handleChange,
+              }}
+            />
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="bg-secondary text-text1 text-[17px] font-semibold pt-2 py-[13px] px-[22px] rounded-lg hover:brightness-75 transition-all"
+            >
+              {loading ? "Loading..." : "Submit"}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </main>
+  );
+};
+
+export default SignupPage;
