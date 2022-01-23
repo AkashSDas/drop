46d73059caf8bcdcf86ee04a61566dda2efd3c9e
AkashSDas <aakashdas368@gmail.com>
2022-01-23 09:26:09 +0530

    setup tailwind css

---
+++ using-redux-toolkit/tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      primary: "#202028",
+      secondary: "#3A8CFF",
+      text1: "#FFFEFF",
+      text2: "#9E9FA7",
+      icon: "#686875",
+      success: "#80B97C",
+      error: "#E15555",
+      card: "#252733",
+    },
+    fontFamily: {
+      gilroy: [
+        "Gilroy",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Open Sans",
+        "Helvetica Neue",
+        "sans-serif",
+      ],
+      muli: [
+        "Muli",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Open Sans",
+        "Helvetica Neue",
+        "sans-serif",
+      ],
+    },
+  },
+  plugins: [],
+};
