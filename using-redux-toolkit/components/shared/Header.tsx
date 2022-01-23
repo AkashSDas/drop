0041483b59bca13d0fc4b49c8bb7667be8ae7fc9
AkashSDas <aakashdas368@gmail.com>
2022-01-23 16:39:14 +0530

    display user img in header

--- using-redux-toolkit/components/shared/Header.tsx
+++ using-redux-toolkit/components/shared/Header.tsx
@@ -16,13 +16,21 @@ const Header = () => {
       <div className={styles.header_inner}>
         <SearchInput />
         {user.token ? (
-          <div className="space-x-8">
+          <div className="space-x-8 flex items-center">
             <TextButton
               text="Logout"
               onClick={async () => {
                 const isLoggedOut = (await dispatch(logoutUser())).payload;
                 if (isLoggedOut) router.push("/");
               }}
+            />
+            <img
+              className="h-[50px] w-[50px] rounded-full object-cover cursor-pointer"
+              src={
+                user?.info?.profilePic?.URL ??
+                "https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80"
+              }
+              alt={user?.info?.username ?? "Unknown User"}
             />
           </div>
         ) : (
