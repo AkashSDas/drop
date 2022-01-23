e77f9485043a2f89cf05d4f32fcc851e79bb99b5
AkashSDas <aakashdas368@gmail.com>
2022-01-23 10:26:57 +0530

    use sidebar in _app.tsx

--- using-redux-toolkit/pages/_app.tsx
+++ using-redux-toolkit/pages/_app.tsx
@@ -1,9 +1,11 @@
+import Sidebar from "@components/shared/Sidebar";
 import { IconlyProvider } from "react-iconly";
 import "../styles/main.scss";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <IconlyProvider set="light" primaryColor="#686875" size="medium">
+      <Sidebar />
       <Component {...pageProps} />
     </IconlyProvider>
   );
