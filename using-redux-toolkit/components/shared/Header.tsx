495bb76c65c803e1528d80cc15467f5afa2ab4d6
AkashSDas <aakashdas368@gmail.com>
2022-01-23 14:57:21 +0530

    conditional render actions in header

--- using-redux-toolkit/components/shared/Header.tsx
+++ using-redux-toolkit/components/shared/Header.tsx
@@ -1,17 +1,26 @@
 import styles from "@style/shared/Header.module.scss";
+import { useAppSelector } from "hooks/store";
 import PrimaryButton from "./PrimaryButton";
 import SearchInput from "./SearchInput";
 import TextButton from "./TextButton";
 
 const Header = () => {
+  const user = useAppSelector((state) => state.user);
+
   return (
     <header className={styles.header}>
       <div className={styles.header_inner}>
         <SearchInput />
-        <div className="space-x-8">
-          <TextButton text="Login" onClick={() => {}} />
-          <PrimaryButton text="Signup" onClick={() => {}} />
-        </div>
+        {user.token ? (
+          <div className="space-x-8">
+            <TextButton text="Logout" onClick={() => {}} />
+          </div>
+        ) : (
+          <div className="space-x-8">
+            <TextButton text="Login" onClick={() => {}} />
+            <PrimaryButton text="Signup" onClick={() => {}} />
+          </div>
+        )}
       </div>
     </header>
   );
