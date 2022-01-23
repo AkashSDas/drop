51a6976a59229cee84d1c59968a1c1c2c1b0bb0d
AkashSDas <aakashdas368@gmail.com>
2022-01-23 15:18:46 +0530

    create logout user thunk

--- using-redux-toolkit/store/logout/slice.ts
+++ using-redux-toolkit/store/logout/slice.ts
@@ -1,4 +1,8 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { removeUserFromLocalStorage } from "lib/local-storage";
+import logout from "lib/service/logout";
+import toast from "react-hot-toast";
+import { initialState as userInitialState, setUser } from "../user/slice";
 
 export interface LogoutState {
   loading: boolean;
@@ -20,3 +24,27 @@ export const logoutSlice = createSlice({
 
 export const { updateLoading } = logoutSlice.actions;
 export default logoutSlice.reducer;
+
+// Thunks
+
+export const logoutUser = createAsyncThunk(
+  "logout/user",
+  async (_, { dispatch }) => {
+    dispatch(updateLoading(true));
+    const response = await logout();
+    dispatch(updateLoading(false));
+    if (response.error) {
+      toast.error(response.error.response.data.msg);
+    } else {
+      if (response.result.isError) {
+        toast.error(response.result.data.msg);
+      } else {
+        toast.success(response.result.data.msg);
+        removeUserFromLocalStorage();
+        dispatch(setUser(userInitialState));
+        return true;
+      }
+    }
+    return false;
+  }
+);
