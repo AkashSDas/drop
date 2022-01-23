b9f6883321f4a776a186730fe98287ae19346269
AkashSDas <aakashdas368@gmail.com>
2022-01-23 10:58:20 +0530

    create search input skeleton and style it

---
+++ using-redux-toolkit/components/shared/SearchInput.tsx
@@ -0,0 +1,19 @@
+import { Search } from "react-iconly";
+import styles from "@style/shared/SearchInput.module.scss";
+
+const SearchInput = () => {
+  return (
+    <div className="flex space-x-4 cursor-pointer">
+      <Search />
+      <input
+        disabled
+        type="text"
+        name="search"
+        placeholder="Search Everything"
+        className={styles.input}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
