3188125678ec6e726731589120d66d4f3a4adec0
AkashSDas <aakashdas368@gmail.com>
2022-01-23 10:40:16 +0530

    delete logo

--- using-redux-toolkit/components/shared/Logo.tsx
+++
@@ -1,6 +0,0 @@
-const Logo = ({ fontSize }: { fontSize: string }) => {
-  const className = `font-gilroy font-extrabold text-secondary text-[${fontSize}]`;
-  return <div className={className}>drop</div>;
-};
-
-export default Logo;
--- using-redux-toolkit/components/shared/Sidebar.tsx
+++ using-redux-toolkit/components/shared/Sidebar.tsx
@@ -1,10 +1,9 @@
 import styles from "@style/shared/Sidebar.module.scss";
-import Logo from "./Logo";
 
 const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
-      <Logo fontSize="30px" />
+      <div className="logo text-[30px]">drop</div>
       <div className="border-b-[1px] border-solid border-[#32333B] w-[168px]"></div>
     </div>
   );
