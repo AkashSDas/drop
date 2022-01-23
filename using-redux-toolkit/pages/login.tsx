8d08b85682866ebdfad9bb817313fa3a0aae0ae6
AkashSDas <aakashdas368@gmail.com>
2022-01-23 14:26:11 +0530

    create login page

---
+++ using-redux-toolkit/pages/login.tsx
@@ -0,0 +1,62 @@
+import IconInput from "@components/shared/IconInput";
+import { Form, Formik } from "formik";
+import { useAppDispatch, useAppSelector } from "hooks/store";
+import { useRouter } from "next/router";
+import { Lock, Message } from "react-iconly";
+import { loginUser } from "store/login/slice";
+
+const LoginPage = () => {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const loading = useAppSelector((state) => state.login.loading);
+
+  const handleSubmit = async (value) => {
+    const isLoggedIn = await dispatch(loginUser(value));
+    if (isLoggedIn) router.push("/");
+  };
+
+  return (
+    <main>
+      <h3>🤝 Login</h3>
+
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={handleSubmit}
+      >
+        {({ values, handleChange, isSubmitting }) => (
+          <Form className="space-y-6">
+            <IconInput
+              icon={<Message />}
+              inputProps={{
+                type: "email",
+                placeholder: "john@example.com",
+                name: "email",
+                value: values.email,
+                onChange: handleChange,
+              }}
+            />
+            <IconInput
+              icon={<Lock />}
+              inputProps={{
+                type: "password",
+                placeholder: "💧💧💧💧💧💧💧💧💧💧💧💧",
+                name: "password",
+                value: values.password,
+                onChange: handleChange,
+              }}
+            />
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="bg-secondary text-text1 text-[17px] font-semibold pt-2 py-[13px] px-[22px] rounded-lg hover:brightness-75 transition-all"
+            >
+              {loading ? "Loading..." : "Submit"}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </main>
+  );
+};
+
+export default LoginPage;
