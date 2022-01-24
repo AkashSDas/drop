9ffad875711fe4cdb877ef04cd45b6a529002ae6
AkashSDas <aakashdas368@gmail.com>
2022-01-24 21:35:58 +0530

    create drops slice

---
+++ using-redux-toolkit/store/drop/slice.ts
@@ -0,0 +1,58 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface IDrop {
+  id: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  user: {
+    id: string;
+    email: string;
+    username: string;
+    profilePic: { id: string; URL: string };
+    role: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+  reactionsOnDrop: { name: string; emoji: string; count: number }[];
+  reacted: null | { reaction: string; id: string };
+}
+
+interface IDropsState {
+  loading: boolean;
+  next: string | null;
+  hasNext: boolean;
+  drops: IDrop[];
+}
+
+const initialState: IDropsState = {
+  loading: false,
+  next: null,
+  hasNext: false,
+  drops: [],
+};
+
+export const dropsSlice = createSlice({
+  name: "drops",
+  initialState,
+  reducers: {
+    updateLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
+    },
+    updateMoreDropsInfo: (
+      state,
+      action: PayloadAction<{ next: string | null; hasNext: boolean }>
+    ) => {
+      state.hasNext = action.payload.hasNext;
+      state.next = action.payload.next;
+    },
+    addDrops: (state, action: PayloadAction<IDrop[]>) => {
+      console.log(action.payload);
+      state.drops = [...state.drops, ...action.payload];
+    },
+  },
+});
+
+export const { updateLoading, addDrops, updateMoreDropsInfo } =
+  dropsSlice.actions;
+export default dropsSlice.reducer;
