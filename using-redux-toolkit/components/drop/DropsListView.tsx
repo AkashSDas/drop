4e90c06e8ab1c60798b30585a65db1370ec38aec
AkashSDas <aakashdas368@gmail.com>
2022-01-24 21:37:03 +0530

    create drops list view

---
+++ using-redux-toolkit/components/drop/DropsListView.tsx
@@ -0,0 +1,45 @@
+import { useAppDispatch, useAppSelector } from "lib/hooks/store";
+import { useEffect } from "react";
+import { fetchDropsThunk } from "store/drop/thunk";
+import DropCard from "./DropCard";
+
+const DropsListView = () => {
+  const dispatch = useAppDispatch();
+  const { drops, loading, hasNext, next } = useAppSelector(
+    (state) => state.drops
+  );
+
+  useEffect(() => {
+    dispatch(fetchDropsThunk());
+  }, []);
+
+  return (
+    <div className="space-y-8">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {drops.map((d, key) => (
+            <>
+              <div className="border-b-[1px] border-solid border-[#32333B]"></div>
+
+              <DropCard
+                key={key}
+                content={d.content}
+                createdAt={d.createdAt}
+                id={d.id}
+                reacted={d.reacted}
+                reactionsOnDrop={d.reactionsOnDrop}
+                updatedAt={d.updatedAt}
+                user={d.user}
+              />
+              <div>{JSON.stringify(d.reacted)}</div>
+            </>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default DropsListView;
