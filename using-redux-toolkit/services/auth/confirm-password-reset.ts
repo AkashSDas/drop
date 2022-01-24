8bc280c438aee8ba7a2a292a117c9dd020fa1864
AkashSDas <aakashdas368@gmail.com>
2022-01-24 11:42:42 +0530

    create confirm password reset service

---
+++ using-redux-toolkit/services/auth/confirm-password-reset.ts
@@ -0,0 +1,16 @@
+import fetchFromAPI from "../";
+
+export interface IConfirmPasswordResetData {
+  url: string;
+  password: string;
+}
+
+const confirmPasswordResetService = async (data: IConfirmPasswordResetData) => {
+  return await fetchFromAPI(data.url, {
+    method: "post",
+    data: { password: data.password },
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+export default confirmPasswordResetService;
