d22d00663394724ea69bc41f8e51c2c7b5b5aabd
AkashSDas <aakashdas368@gmail.com>
2022-01-24 11:34:36 +0530

    refactor lib/index.ts

--- using-redux-toolkit/lib/index.ts
+++ using-redux-toolkit/lib/index.ts
@@ -12,17 +12,3 @@ export const monthNames = [
   "Nov",
   "Dec",
 ];
-
-interface IRunAsync {
-  result: any;
-  error: any;
-}
-
-export const runAsync = async (promise: Promise<any>): Promise<IRunAsync> => {
-  try {
-    const result = await promise;
-    return { result: result, error: null };
-  } catch (err) {
-    return { result: null, error: err };
-  }
-};
