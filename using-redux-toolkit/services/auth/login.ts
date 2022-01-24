88039b549890f1dc1c9ca9e1936c6154f038c560
AkashSDas <aakashdas368@gmail.com>
2022-01-24 11:34:09 +0530

    create login service

---
+++ using-redux-toolkit/services/auth/login.ts
@@ -0,0 +1,16 @@
+import fetchFromAPI from "../";
+
+export interface ILoginData {
+  email: string;
+  password: string;
+}
+
+const loginService = async (data: ILoginData) => {
+  return await fetchFromAPI("/user/login", {
+    method: "post",
+    data,
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+export default loginService;
