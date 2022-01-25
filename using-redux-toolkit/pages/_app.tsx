6577d1be10bc0f9fec621c0f4b83ce93354d32f8
AkashSDas <aakashdas368@gmail.com>
2022-01-25 16:39:09 +0530

    use create drop form in _app.tsx

--- using-redux-toolkit/pages/_app.tsx
+++ using-redux-toolkit/pages/_app.tsx
@@ -6,12 +6,14 @@ import { Provider } from "react-redux";
 import "../styles/main.scss";
 import store from "../store";
 import AuthWrapper from "@components/shared/AuthWrapper";
+import CreateDropForm from "@components/shared/CreateDropForm";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
       <AuthWrapper>
         <IconlyProvider set="light" primaryColor="#686875" size="medium">
+          <CreateDropForm />
           <Header />
           <Sidebar />
           <Component {...pageProps} />
