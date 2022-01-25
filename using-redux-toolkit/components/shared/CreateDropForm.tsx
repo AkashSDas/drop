7fe1351f4738c0d1c7805aab46fa33f21e5c351e
AkashSDas <aakashdas368@gmail.com>
2022-01-25 17:03:31 +0530

    update create drop form state

--- using-redux-toolkit/components/shared/CreateDropForm.tsx
+++ using-redux-toolkit/components/shared/CreateDropForm.tsx
@@ -1,20 +1,25 @@
 import { Form, Formik } from "formik";
-import { Search } from "react-iconly";
+import { useAppDispatch, useAppSelector } from "lib/hooks/store";
+import { Send } from "react-iconly";
+import { updateIsOpen } from "store/create-drop-form/slice";
 
 const CreateDropForm = () => {
-  return (
+  const isOpen = useAppSelector((state) => state.createDropForm.isOpen);
+  const dispatch = useAppDispatch();
+
+  return !isOpen ? null : (
     <div className="fixed h-screen w-screen bg-[#000] bg-opacity-90 z-10">
       <div className="bg-primary fixed top-0 left-0 w-full h-[93px] flex justify-center items-center">
         <Formik initialValues={{ search: "" }} onSubmit={() => {}}>
           {({ values, handleChange }) => (
             <Form className="w-full flex justify-center items-center space-x-8">
               <button type="submit">
-                <Search />
+                <Send primaryColor="rgb(58 140 255)" />
               </button>
               <input
                 type="text"
-                className="bg-primary w-[45%] outline-none"
-                placeholder="Search Everything"
+                className="bg-primary w-[45%] outline-none text-text1 font-semibold"
+                placeholder="Drop your bomb!"
                 name="search"
                 value={values.search}
                 onChange={handleChange}
@@ -22,6 +27,7 @@ const CreateDropForm = () => {
               <button
                 type="button"
                 className="bg-secondary text-text1 text-[17px] font-semibold pt-2 py-[13px] px-[22px] rounded-lg hover:brightness-75 transition-all"
+                onClick={() => dispatch(updateIsOpen(false))}
               >
                 Close
               </button>
@@ -29,6 +35,10 @@ const CreateDropForm = () => {
           )}
         </Formik>
       </div>
+      <div
+        className="w-full h-full z-[9] cursor-pointer"
+        onClick={() => dispatch(updateIsOpen(false))}
+      ></div>
     </div>
   );
 };
