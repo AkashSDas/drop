1d700ff480efeb3e7ca9bbaba5f40be06ff7fe0c
AkashSDas <aakashdas368@gmail.com>
2022-01-25 19:05:21 +0530

    wrap inifinite scroll in drop listview

--- using-redux-toolkit/components/drop/DropsListView.tsx
+++ using-redux-toolkit/components/drop/DropsListView.tsx
@@ -1,5 +1,6 @@
 import { useAppDispatch, useAppSelector } from "lib/hooks/store";
 import { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchDropsThunk } from "store/drop/thunk";
 import DropCard from "./DropCard";
 import DropsListViewLoading from "./DropsListViewLoading";
@@ -7,7 +8,7 @@ import DropsListViewLoading from "./DropsListViewLoading";
 const DropsListView = () => {
   const dispatch = useAppDispatch();
   const token = useAppSelector((state) => state.user.token);
-  const { drops, loading } = useAppSelector((state) => state.drops);
+  const { drops, loading, hasNext } = useAppSelector((state) => state.drops);
 
   useEffect(() => {
     dispatch(fetchDropsThunk(true));
@@ -18,7 +19,14 @@ const DropsListView = () => {
       {loading ? (
         <DropsListViewLoading />
       ) : (
-        <>
+        <InfiniteScroll
+          dataLength={drops.length}
+          next={() => {}}
+          hasMore={hasNext}
+          loader={<DropsListViewLoading />}
+          endMessage={<div className="font-semibold text-[23px]">The End</div>}
+          className="space-y-8"
+        >
           {drops.map((d, key) => (
             <div key={key} className="space-y-8">
               <div className="border-b-[1px] border-solid border-[#32333B]"></div>
@@ -34,7 +42,7 @@ const DropsListView = () => {
               />
             </div>
           ))}
-        </>
+        </InfiniteScroll>
       )}
     </div>
   );
