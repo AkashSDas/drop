f16a20af736c3274d91a0b1a8dfa75da0365342e
AkashSDas <aakashdas368@gmail.com>
2022-01-25 17:03:55 +0530

    add fixed position depending on create drop form state

--- using-redux-toolkit/pages/index.tsx
+++ using-redux-toolkit/pages/index.tsx
@@ -1,8 +1,11 @@
 import DropsListView from "@components/drop/DropsListView";
+import { useAppSelector } from "lib/hooks/store";
 
 const IndexPage = () => {
+  const isOpen = useAppSelector((state) => state.createDropForm.isOpen);
+
   return (
-    <main>
+    <main className={isOpen ? `fixed` : ""}>
       <h3>💧 Drops</h3>
       <DropsListView />
     </main>
