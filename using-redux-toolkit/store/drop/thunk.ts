bb87913b67ff2e1485a97a6837e6609a96648ad8
AkashSDas <aakashdas368@gmail.com>
2022-01-26 14:33:02 +0530

    create thunks for updating reaction on independent drop card

--- using-redux-toolkit/store/drop/thunk.ts
+++ using-redux-toolkit/store/drop/thunk.ts
@@ -1,8 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import toast from "react-hot-toast";
 import fetchDropService from "services/drop/fetch-drop";
+import reactOnDropService from "services/reaction/react-on-drop";
+import toggleReactionOnDropService from "services/reaction/toggle-reaction";
+import unReactDropService from "services/reaction/unreact-drop";
 import { IDrop } from "store/drops/slice";
-import { updateDrop, updateLoading } from "./slice";
+import {
+  addDropReaction,
+  toggleDropReacted,
+  unReactDropReaction,
+  updateDrop,
+  updateLoading,
+  updateReactionLoading,
+} from "./slice";
 
 export const fetchDropThunk = createAsyncThunk(
   "drop/updateDrop",
@@ -58,5 +68,103 @@ export const fetchDropThunk = createAsyncThunk(
 
       dispatch(updateDrop(dropInfo));
     }
+  }
+);
+
+export const toggleReactionOnDropThunk = createAsyncThunk(
+  "drop/toggleReaction",
+  async (
+    {
+      dropId,
+      reaction,
+      oldReaction,
+    }: { dropId: string; reaction: string; oldReaction: string },
+    { dispatch, getState }
+  ) => {
+    const token = (getState() as any).user.token;
+    if (!token) {
+      toast.error("You are not logged in");
+      return;
+    }
+
+    dispatch(
+      toggleDropReacted({
+        reaction,
+        id: "",
+        oldReaction,
+        countUpdated: false,
+      })
+    );
+
+    dispatch(updateReactionLoading(true));
+    const response = await toggleReactionOnDropService(token, dropId, reaction);
+    dispatch(updateReactionLoading(false));
+
+    // toggle drop of dropId reacted state to update
+    const newReaction = response.data.reaction;
+    dispatch(
+      toggleDropReacted({
+        reaction: newReaction.reaction,
+        id: newReaction.id,
+        oldReaction,
+        countUpdated: true,
+      })
+    );
+  }
+);
+
+export const reactOnDropThunk = createAsyncThunk(
+  "drop/react",
+  async (
+    { dropId, reaction }: { dropId: string; reaction: string },
+    { dispatch, getState }
+  ) => {
+    const token = (getState() as any).user.token;
+    if (!token) {
+      toast.error("You are not logged in");
+      return;
+    }
+
+    dispatch(
+      addDropReaction({
+        reaction,
+        id: "",
+        countUpdated: false,
+      })
+    );
+
+    dispatch(updateReactionLoading(true));
+    const response = await reactOnDropService(token, dropId, reaction);
+    dispatch(updateReactionLoading(false));
+
+    // toggle drop of dropId reacted state to update
+    const newReaction = response.data.reaction;
+    dispatch(
+      addDropReaction({
+        reaction: newReaction.reaction,
+        id: newReaction.id,
+        countUpdated: true,
+      })
+    );
+  }
+);
+
+export const unReactDropReactionThunk = createAsyncThunk(
+  "drop/unreact",
+  async (
+    { dropId, reaction }: { dropId: string; reaction: string },
+    { dispatch, getState }
+  ) => {
+    const token = (getState() as any).user.token;
+    if (!token) {
+      toast.error("You are not logged in");
+      return;
+    }
+
+    dispatch(unReactDropReaction({ reaction, countUpdated: false }));
+    dispatch(updateReactionLoading(true));
+    await unReactDropService(token, dropId);
+    dispatch(updateReactionLoading(false));
+    dispatch(unReactDropReaction({ reaction, countUpdated: true }));
   }
 );
