c52619f518b377d59ddf6b47755f00db4627b6f1
AkashSDas <aakashdas368@gmail.com>
2022-01-26 23:16:12 +0530

    create update comment page

---
+++ using-redux-toolkit/pages/comment/[commentId].tsx
@@ -0,0 +1,69 @@
+import DropComment from "@components/drop/DropComment";
+import { Form, Formik } from "formik";
+import { useAppDispatch, useAppSelector } from "lib/hooks/store";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { Send } from "react-iconly";
+import { updateLoading } from "store/comment/slice";
+import { fetchCommentThunk } from "store/comment/thunk";
+
+const UpdateDropCommentPage = () => {
+  const router = useRouter();
+  const { loading, comment } = useAppSelector((state) => state.comment);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (router.isReady) {
+      const { commentId } = router.query;
+      dispatch(fetchCommentThunk(commentId as string));
+    } else {
+      dispatch(updateLoading(true));
+    }
+  }, [router.isReady]);
+
+  if (loading || !comment) return <main>Loading...</main>;
+  return (
+    <main>
+      <DropComment comment={comment} hideActions />
+
+      <Formik
+        initialValues={{ content: comment.content }}
+        onSubmit={async (value, { resetForm }) => {
+          // if (token) {
+          //   await dispatch(
+          //     createCommentThunk({
+          //       token,
+          //       dropId: drop.id,
+          //       content: value.content,
+          //     })
+          //   );
+          //   resetForm();
+          // } else {
+          //   toast.error("You are not logged in");
+          // }
+        }}
+      >
+        {({ values, handleChange, isSubmitting }) => (
+          <Form className="w-full flex space-x-4 mb-3">
+            <input
+              type="text"
+              className="outline-none bg-card rounded-lg w-full px-6 py-[14px] placeholder:text-text2 text-text1 placeholder:opacity-60"
+              onChange={handleChange}
+              name="content"
+              placeholder="Updated comment"
+              value={values.content}
+            />
+            <button
+              className={isSubmitting ? `animate-spin` : ""}
+              disabled={isSubmitting}
+            >
+              <Send primaryColor="#3A8CFF" />
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </main>
+  );
+};
+
+export default UpdateDropCommentPage;
