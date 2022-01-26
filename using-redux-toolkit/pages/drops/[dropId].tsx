2c3ee0c920c69886c2367d977199103820b0aa33
AkashSDas <aakashdas368@gmail.com>
2022-01-26 17:56:25 +0530

    fetch and update drop comments

--- using-redux-toolkit/pages/drops/[dropId].tsx
+++ using-redux-toolkit/pages/drops/[dropId].tsx
@@ -1,43 +1,62 @@
 import DropCardLoadng from "@components/drop/DropCardLoading";
+import DropComments from "@components/drop/DropComments";
 import IndependentDropCard from "@components/drop/IndependentDropCard";
 import { useAppDispatch, useAppSelector } from "lib/hooks/store";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { updateLoading } from "store/drop/slice";
+import { fetchDropCommentsThunk } from "store/drop-comments/thunk";
+import { updateLoading as updateDropLoading } from "store/drop/slice";
+import { updateLoading as updateCommentsLoading } from "store/drop-comments/slice";
 import { fetchDropThunk } from "store/drop/thunk";
+import DropCommentsLoading from "@components/drop/DropCommentsLoading";
 
 const DropPage = () => {
   const router = useRouter();
-  const { loading, drop } = useAppSelector((state) => state.drop);
+  const { loading: dropLoading, drop } = useAppSelector((state) => state.drop);
+  const { loading: commentsLoading, comments } = useAppSelector(
+    (state) => state.dropComments
+  );
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (router.isReady) {
       const { dropId } = router.query;
       dispatch(fetchDropThunk(dropId as string));
+      dispatch(
+        fetchDropCommentsThunk({ dropId: dropId as string, init: true })
+      );
     } else {
-      dispatch(updateLoading(true));
+      dispatch(updateDropLoading(true));
+      dispatch(updateCommentsLoading(true));
     }
   }, [router.isReady]);
 
-  if (loading || !drop)
-    return (
-      <main>
-        <DropCardLoadng />
-      </main>
-    );
-
   return (
     <main>
-      <IndependentDropCard
-        content={drop.content}
-        createdAt={drop.createdAt}
-        id={drop.id}
-        reacted={drop.reacted}
-        reactionsOnDrop={drop.reactionsOnDrop}
-        updatedAt={drop.updatedAt}
-        user={drop.user}
-      />
+      {dropLoading || !drop ? (
+        <DropCardLoadng />
+      ) : (
+        <IndependentDropCard
+          content={drop.content}
+          createdAt={drop.createdAt}
+          id={drop.id}
+          reacted={drop.reacted}
+          reactionsOnDrop={drop.reactionsOnDrop}
+          updatedAt={drop.updatedAt}
+          user={drop.user}
+        />
+      )}
+
+      <section className="space-y-4">
+        <h4>Comments</h4>
+        <div className="border-b-[1px] border-solid border-[#32333B]"></div>
+
+        {commentsLoading || !comments ? (
+          <DropCommentsLoading />
+        ) : (
+          <DropComments />
+        )}
+      </section>
     </main>
   );
 };
