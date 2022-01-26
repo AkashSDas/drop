389b1f137e6477e3ad7e93e114f3c0bd9dba0f29
AkashSDas <aakashdas368@gmail.com>
2022-01-26 17:55:11 +0530

    create drop comments loading

---
+++ using-redux-toolkit/components/drop/DropCommentsLoading.tsx
@@ -0,0 +1,13 @@
+import DropCommentLoading from "./DropCommentLoading";
+
+const DropCommentsLoading = () => {
+  return (
+    <div className="space-y-8">
+      <DropCommentLoading />
+      <DropCommentLoading />
+      <DropCommentLoading />
+    </div>
+  );
+};
+
+export default DropCommentsLoading;
