24ba0dc00b8e9ae262e3e3a55925781aee1b919a
AkashSDas <aakashdas368@gmail.com>
2022-01-26 23:15:30 +0530

    create fetch comment thunk

---
+++ using-redux-toolkit/store/comment/thunk.ts
@@ -0,0 +1,46 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+import getCommentService from "services/comment/get-comment";
+import { IComment } from "store/drop-comments/slice";
+import { updateComment, updateLoading } from "./slice";
+
+export const fetchCommentThunk = createAsyncThunk(
+  "comment/fetch",
+  async (commentId: string, { dispatch }) => {
+    dispatch(updateLoading(true));
+    const response = await getCommentService(commentId);
+    dispatch(updateLoading(false));
+    if (response.isError) toast.error(response.msg);
+    else {
+      const comment = response.data.comment;
+      const commented = false; // this is not needed so not getting it from backend
+
+      const data: IComment = {
+        id: comment.id,
+        dropId: comment.drop,
+        content: comment.content,
+        createdAt: comment.createdAt,
+        updatedAt: comment.updatedAt,
+        user: {
+          id: comment.user.id,
+          email: comment.user.email,
+          username: comment.user.username,
+          profilePic: comment.user.profilePic
+            ? {
+                id: comment.user.profilePic.id,
+                URL: comment.user.profilePic.URL,
+              }
+            : {
+                id: "",
+                URL: "https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80",
+              },
+          role: comment.user.role,
+          createdAt: comment.user.createdAt,
+          updatedAt: comment.user.updatedAt,
+        },
+        commented: commented,
+      };
+      dispatch(updateComment(data));
+    }
+  }
+);
