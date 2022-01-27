059d09aa05ad67dd1500df409928f0236191d529
AkashSDas <aakashdas368@gmail.com>
2022-01-27 17:21:13 +0530

    update url check fetch user drops

--- using-redux-toolkit/services/profile/fetch-user-drops-paginated.ts
+++ using-redux-toolkit/services/profile/fetch-user-drops-paginated.ts
@@ -12,10 +12,10 @@ const fetchUserDropsPaginatedService = async (
 ) => {
   let url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/drop/user/${params.userId}?`;
   url = `${url}limit=${params.limit ?? 10}`;
-  if (params.next && params.userId) {
+  if (params.next && params.selfUserId) {
     url = `${url}&next=${params.next}&user=${params.selfUserId}`;
   } else if (params.next) url = `${url}&next=${params.next}`;
-  else if (params.userId) url = `${url}&user=${params.selfUserId}`;
+  else if (params.selfUserId) url = `${url}&user=${params.selfUserId}`;
 
   return await fetchFromAPI(url, { method: "get" });
 };
