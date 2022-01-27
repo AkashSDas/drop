4b7f1c136ee0b498a7e27b0329d57ccd221f810b
AkashSDas <aakashdas368@gmail.com>
2022-01-27 10:31:11 +0530

    add tab content in profile drops page

--- using-redux-toolkit/pages/profile/[profileId]/drops.tsx
+++ using-redux-toolkit/pages/profile/[profileId]/drops.tsx
@@ -1,11 +1,32 @@
+import Drops from "@components/profile/Drops";
 import ProfileTabContainer from "@components/profile/ProfileTab";
 import UserInfo from "@components/profile/UserInfo";
+import { AnimatePresence, motion } from "framer-motion";
+import { useAppSelector } from "lib/hooks/store";
 
 const ProfileDropsPage = () => {
+  const currentTab = useAppSelector((state) => state.profile.currentTab);
+
+  const displayContent = () => {
+    if (currentTab === "drop") return <Drops />;
+    return null;
+  };
+
   return (
     <main>
       <UserInfo />
       <ProfileTabContainer />
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={currentTab}
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 30 }}
+          exit={{ opacity: 0, y: -30 }}
+          transition={{ duration: 0.15 }}
+        >
+          {displayContent()}
+        </motion.div>
+      </AnimatePresence>
     </main>
   );
 };
