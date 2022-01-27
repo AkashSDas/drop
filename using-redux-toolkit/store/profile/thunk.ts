aa77b8d81a144b9bbc2f760d5ddfd3fa21bda3c6
AkashSDas <aakashdas368@gmail.com>
2022-01-27 12:21:30 +0530

    create fetch more thunk

--- using-redux-toolkit/store/profile/thunk.ts
+++ using-redux-toolkit/store/profile/thunk.ts
@@ -8,6 +8,7 @@ import { IDrop } from "store/drops/slice";
 import {
   initDropsAdd,
   IUser,
+  pushDrops,
   updateFollowingStatus,
   updateLoadingDrops,
   updateLoadingProfile,
@@ -176,6 +177,76 @@ export const fetchUserDropsThunk = createAsyncThunk(
         })
       );
       dispatch(initDropsAdd(drops));
+    }
+  }
+);
+
+export const fetchUserMoreDropsThunk = createAsyncThunk(
+  "profile/fetchMoreDrops",
+  async (userId: string, { dispatch, getState }) => {
+    const selfUserId = (getState() as any).user.info.id;
+    const next = (getState() as any).profile.dropsNext;
+    const limit = 4;
+    const response = await fetchUserDropsPaginatedService({
+      userId,
+      next,
+      limit,
+      selfUserId,
+    });
+
+    if (response.isError) toast.error(response.msg);
+    else {
+      // transform drop
+      let drops: IDrop[] = [];
+      for (let i = 0; i < response.data.drops.length; i++) {
+        const drop = response.data.drops[i].drop;
+        const reactionsOnDrop = response.data.drops[i].reactionsOnDrop;
+        const reacted = response.data.drops[i].reacted;
+
+        let reactionsOnDropArr: {
+          name: string;
+          emoji: string;
+          count: number;
+        }[] = [];
+        for (const reaction in reactionsOnDrop) {
+          reactionsOnDropArr.push({
+            name: reactionsOnDrop[reaction].name,
+            emoji: reactionsOnDrop[reaction].emoji,
+            count: reactionsOnDrop[reaction].count,
+          });
+        }
+
+        drops.push({
+          id: drop.id,
+          content: drop.content,
+          createdAt: drop.createdAt,
+          updatedAt: drop.updatedAt,
+          user: {
+            id: drop.user.id,
+            email: drop.user.email,
+            username: drop.user.username,
+            profilePic: {
+              id: drop.user.profilePic.id,
+              URL: drop.user.profilePic.URL,
+            },
+            role: drop.user.role,
+            createdAt: drop.user.createdAt,
+            updatedAt: drop.user.updatedAt,
+          },
+          reactionsOnDrop: reactionsOnDropArr,
+          reacted: !reacted
+            ? null
+            : { reaction: reacted.reaction, id: reacted.id },
+        });
+      }
+
+      dispatch(
+        updateMoreDropsInfo({
+          next: response.data.next,
+          hasNext: response.data.hasNext,
+        })
+      );
+      dispatch(pushDrops(drops));
     }
   }
 );
