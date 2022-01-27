f169a80391c9a7fee69b8327977109c459616365
AkashSDas <aakashdas368@gmail.com>
2022-01-27 17:24:56 +0530

    creae followers component

---
+++ using-redux-toolkit/components/profile/Followers.tsx
@@ -0,0 +1,39 @@
+import { useAppDispatch, useAppSelector } from "lib/hooks/store";
+import { useEffect } from "react";
+import { fetchUserFollowersThunk } from "store/profile/thunk";
+import FollowerInfo from "./FollowerInfo";
+import { UserInfoLoading } from "./UserInfo";
+
+const Followers = () => {
+  const dispatch = useAppDispatch();
+  const token = useAppSelector((state) => state.user.token);
+  const { followers, loadingFollowers, followersHasNext, user } =
+    useAppSelector((state) => state.profile);
+
+  useEffect(() => {
+    if (user.id) {
+      dispatch(fetchUserFollowersThunk({ init: true, userId: user.id }));
+    }
+  }, [token, user.id]);
+
+  return (
+    <div className="space-y-8">
+      {loadingFollowers ? (
+        <>
+          <UserInfoLoading />
+          <UserInfoLoading />
+          <UserInfoLoading />
+          <UserInfoLoading />
+        </>
+      ) : (
+        <>
+          {followers.map((f, key) => (
+            <FollowerInfo info={f} key={key} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Followers;
