1027556ca69b09078c8fc6ce85fb7e9a3dd44eff
AkashSDas <aakashdas368@gmail.com>
2022-01-27 09:43:27 +0530

    update url

--- using-redux-toolkit/services/profile/fetch-profile.ts
+++ using-redux-toolkit/services/profile/fetch-profile.ts
@@ -2,7 +2,7 @@ import fetchFromAPI from "lib/services";
 
 const fetchProfileService = async (userId: string, selfUserId?: string) => {
   let url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/user/${userId}`;
-  if (selfUserId) url = `${url}?${selfUserId}`;
+  if (selfUserId) url = `${url}?user=${selfUserId}`;
   return await fetchFromAPI(url, { method: "get" });
 };
 
