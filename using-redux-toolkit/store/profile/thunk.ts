780386de1f922ff048cbfc2a743d59ebdbe449bf
AkashSDas <aakashdas368@gmail.com>
2022-01-27 12:03:31 +0530

    create fetch user drops thunk

--- using-redux-toolkit/store/profile/thunk.ts
+++ using-redux-toolkit/store/profile/thunk.ts
@@ -1,13 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import toast from "react-hot-toast";
 import fetchProfileService from "services/profile/fetch-profile";
+import fetchUserDropsPaginatedService from "services/profile/fetch-user-drops-paginated";
 import followUserService from "services/profile/follow-user";
 import unFollowUserService from "services/profile/unfollow-user";
+import { IDrop } from "store/drops/slice";
 import {
+  initDropsAdd,
   IUser,
   updateFollowingStatus,
+  updateLoadingDrops,
   updateLoadingProfile,
   updateLoadingUserFollow,
+  updateMoreDropsInfo,
   updateProfileAndSelfRelation,
   updateUser,
 } from "./slice";
@@ -96,6 +101,81 @@ export const unFollowUserThunk = createAsyncThunk(
       dispatch(
         updateFollowingStatus({ relationshipId: null, following: false })
       );
+    }
+  }
+);
+
+export const fetchUserDropsThunk = createAsyncThunk(
+  "profile/dropsAdded",
+  async (
+    { init, userId }: { init: boolean; userId: string },
+    { dispatch, getState }
+  ) => {
+    dispatch(updateLoadingDrops(true));
+
+    const selfUserId = (getState() as any).user.info.id;
+    const next = (getState() as any).drops.next;
+    const limit = 4;
+
+    const response = await fetchUserDropsPaginatedService(
+      init ? { userId, limit, selfUserId } : { userId, next, limit, selfUserId }
+    );
+
+    dispatch(updateLoadingDrops(false));
+
+    if (response.isError) toast.error(response.msg);
+    else {
+      // transform drop
+      let drops: IDrop[] = [];
+      for (let i = 0; i < response.data.drops.length; i++) {
+        const drop = response.data.drops[i].drop;
+        const reactionsOnDrop = response.data.drops[i].reactionsOnDrop;
+        const reacted = response.data.drops[i].reacted;
+
+        let reactionsOnDropArr: {
+          name: string;
+          emoji: string;
+          count: number;
+        }[] = [];
+        for (const reaction in reactionsOnDrop) {
+          reactionsOnDropArr.push({
+            name: reactionsOnDrop[reaction].name,
+            emoji: reactionsOnDrop[reaction].emoji,
+            count: reactionsOnDrop[reaction].count,
+          });
+        }
+
+        drops.push({
+          id: drop.id,
+          content: drop.content,
+          createdAt: drop.createdAt,
+          updatedAt: drop.updatedAt,
+          user: {
+            id: drop.user.id,
+            email: drop.user.email,
+            username: drop.user.username,
+            profilePic: {
+              id: drop.user.profilePic.id,
+              URL: drop.user.profilePic.URL,
+            },
+            role: drop.user.role,
+            createdAt: drop.user.createdAt,
+            updatedAt: drop.user.updatedAt,
+          },
+          reactionsOnDrop: reactionsOnDropArr,
+          reacted: !reacted
+            ? null
+            : { reaction: reacted.reaction, id: reacted.id },
+        });
+      }
+
+      dispatch(
+        updateMoreDropsInfo({
+          next: response.data.next,
+          hasNext: response.data.hasNext,
+        })
+      );
+      dispatch(initDropsAdd(drops));
     }
   }
 );
