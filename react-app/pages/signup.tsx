5651da1d0a9f4475f91eed4615d5b73316973842
AkashSDas <aakashdas368@gmail.com>
2022-02-05 20:03:30 +0530

    dry up signup page

--- react-app/pages/signup.tsx
+++ react-app/pages/signup.tsx
@@ -1,77 +1,12 @@
-import IconInput from "@components/shared/IconInput";
-import { Form, Formik } from "formik";
-import { useAppDispatch, useAppSelector } from "lib/hooks/store";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { Lock, Message, User } from "react-iconly";
-import { signupThunk } from "store/signup/thunk";
+import SignupForm from "@components/signup/SignupForm";
+import SignupHelpLinks from "@components/signup/SignupHelpLinks";
 
 const SignupPage = () => {
-  const router = useRouter();
-  const dispatch = useAppDispatch();
-  const loading = useAppSelector((state) => state.signup.loading);
-
-  const handleSubmit = async (value) => {
-    const isSignedIn = (await dispatch(signupThunk(value))).payload;
-    if (isSignedIn) router.push("/");
-  };
-
   return (
     <main>
       <h3>🤝 Signup</h3>
-
-      <Formik
-        initialValues={{ username: "", email: "", password: "" }}
-        onSubmit={handleSubmit}
-      >
-        {({ values, handleChange, isSubmitting }) => (
-          <Form className="space-y-6">
-            <IconInput
-              icon={<User />}
-              inputProps={{
-                type: "text",
-                placeholder: "John Smith",
-                name: "username",
-                value: values.username,
-                onChange: handleChange,
-              }}
-            />
-            <IconInput
-              icon={<Message />}
-              inputProps={{
-                type: "email",
-                placeholder: "john@example.com",
-                name: "email",
-                value: values.email,
-                onChange: handleChange,
-              }}
-            />
-            <IconInput
-              icon={<Lock />}
-              inputProps={{
-                type: "password",
-                placeholder: "💧💧💧💧💧💧💧💧💧💧💧💧",
-                name: "password",
-                value: values.password,
-                onChange: handleChange,
-              }}
-            />
-            <button
-              type="submit"
-              disabled={isSubmitting}
-              className="bg-secondary text-text1 text-[17px] font-semibold pt-2 py-[13px] px-[22px] rounded-lg hover:brightness-75 transition-all"
-            >
-              {loading ? "Loading..." : "Submit"}
-            </button>
-          </Form>
-        )}
-      </Formik>
-
-      <div className="space-x-4 text-secondary">
-        <Link href="/login">
-          <span className="cursor-pointer">Already have an account?</span>
-        </Link>
-      </div>
+      <SignupForm />
+      <SignupHelpLinks />
     </main>
   );
 };
