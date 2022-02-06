3466b10666ae5804683241b493a8942722879895
AkashSDas <aakashdas368@gmail.com>
2022-02-06 10:49:40 +0530

    use rolling animation

--- react-app/components/signup/SignupButton.tsx
+++ react-app/components/signup/SignupButton.tsx
@@ -1,12 +1,13 @@
 import { useAppSelector } from "lib/hooks/store";
 import styles from "@style/signup/SignupButton.module.scss";
+import RollingAnimation from "@components/animation/RollingAnimation";
 
 const SignupButton = ({ isSubmitting }: { isSubmitting: boolean }) => {
   const loading = useAppSelector((state) => state.signup.loading);
 
   return (
     <button type="submit" disabled={isSubmitting} className={styles.btn}>
-      {loading ? "Loading..." : "Submit"}
+      <RollingAnimation>{loading ? "Loading..." : "Submit"}</RollingAnimation>
     </button>
   );
 };
--- react-app/pages/signup.tsx
+++ react-app/pages/signup.tsx
@@ -1,4 +1,6 @@
 import RevealAnimation from "@components/animation/RevealAnimation";
+import RollingAnimation from "@components/animation/RollingAnimation";
+import TextRollAnimation from "@components/animation/TextRollAnimation";
 import SignupForm from "@components/signup/SignupForm";
 import SignupHelpLinks from "@components/signup/SignupHelpLinks";
 
@@ -6,7 +8,9 @@ const SignupPage = () => {
   return (
     <main>
       <RevealAnimation duration={1} rotate={10} y={30}>
-        <h3>🤝 Signup</h3>
+        <RollingAnimation>
+          <h3>🤝 Signup</h3>
+        </RollingAnimation>
       </RevealAnimation>
       <SignupForm />
       <RevealAnimation duration={1} rotate={10} y={30}>
