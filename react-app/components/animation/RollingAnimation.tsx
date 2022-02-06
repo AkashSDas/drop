1f389e5ea0eb8cc7b1c4cafa912af6ea6fa25be0
AkashSDas <aakashdas368@gmail.com>
2022-02-06 20:01:19 +0530

    update animation curve

--- react-app/components/animation/RollingAnimation.tsx
+++ react-app/components/animation/RollingAnimation.tsx
@@ -1,10 +1,10 @@
 const RollingAnimation = ({ children }) => {
   return (
     <div className="overflow-hidden relative group">
-      <div className="relative translate-y-[0%] group-hover:translate-y-[-110%] ease-out duration-300">
+      <div className="relative translate-y-[0%] group-hover:translate-y-[-110%] expo-in-ease-out duration-300">
         {children}
       </div>
-      <div className="absolute translate-y-[110%] group-hover:translate-y-[-100%] skew-y-[10deg] group-hover:skew-y-[0deg] ease-out duration-300">
+      <div className="absolute translate-y-[110%] group-hover:translate-y-[-100%] skew-y-[10deg] group-hover:skew-y-[0deg] expo-in-ease-out duration-300">
         {children}
       </div>
     </div>
