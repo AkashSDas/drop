1ae9d6fae0fe043288ae7d91e36bc63d6244b23c
AkashSDas <aakashdas368@gmail.com>
2022-02-07 10:53:12 +0530

    refactor forgot password page

--- react-app/pages/forgot-password.tsx
+++ react-app/pages/forgot-password.tsx
@@ -1,49 +1,20 @@
-import { Form, Formik } from "formik";
-import { useAppDispatch, useAppSelector } from "lib/hooks/store";
-import { useRouter } from "next/router";
-import { Message } from "react-iconly";
-import { forgotPasswordThunk } from "store/forgot-password/thunk";
-
-import IconInput from "@components/shared/IconInput";
+import LoadAnimation from "@components/animation/LoadAnimation";
+import RevealAnimation from "@components/animation/RevealAnimation";
+import RollingAnimation from "@components/animation/RollingAnimation";
+import ForgotPasswordForm from "@components/forgot-password/ForgotPasswordForm";
 
 const ForgotPasswordPage = () => {
-  const router = useRouter();
-  const dispatch = useAppDispatch();
-  const loading = useAppSelector((state) => state.forgotPassword.loading);
-
-  const handleSubmit = async (value) => {
-    const redirect = (await dispatch(forgotPasswordThunk(value))).payload;
-    if (redirect) router.push("/confirm-password-reset");
-  };
-
   return (
-    <main>
-      <h3>🤝 Forgot Password</h3>
-
-      <Formik initialValues={{ email: "" }} onSubmit={handleSubmit}>
-        {({ values, handleChange, isSubmitting }) => (
-          <Form className="space-y-6">
-            <IconInput
-              icon={<Message />}
-              inputProps={{
-                type: "email",
-                placeholder: "john@example.com",
-                name: "email",
-                value: values.email,
-                onChange: handleChange,
-              }}
-            />
-            <button
-              type="submit"
-              disabled={isSubmitting}
-              className="bg-secondary text-text1 text-[17px] font-semibold pt-2 py-[13px] px-[22px] rounded-lg hover:brightness-75 transition-all"
-            >
-              {loading ? "Loading..." : "Submit"}
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </main>
+    <LoadAnimation delay={0.2} duration={1} initialY={60}>
+      <main>
+        <RevealAnimation rotate={10} y={60} duration={1}>
+          <RollingAnimation>
+            <h3>🤝 Forgot Password</h3>
+          </RollingAnimation>
+        </RevealAnimation>
+        <ForgotPasswordForm />
+      </main>
+    </LoadAnimation>
   );
 };
 
