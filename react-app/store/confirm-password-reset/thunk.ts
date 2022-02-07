52125d98fcc12c5fdb6714334c99293d3f72c216
AkashSDas <aakashdas368@gmail.com>
2022-02-07 11:13:57 +0530

    refactor confirm password reset thunk

--- react-app/store/confirm-password-reset/thunk.ts
+++ react-app/store/confirm-password-reset/thunk.ts
@@ -3,14 +3,10 @@ import confirmPasswordResetService, { IConfirmPasswordResetData } from "services
 
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-import { updateLoading } from "./slice";
-
-export const confirmPasswordResetThunk = createAsyncThunk(
-  "confirmPasswordReset/user",
+export const confirmPasswordReset = createAsyncThunk(
+  "confirmPasswordReset/resetPassword",
   async (data: IConfirmPasswordResetData, { dispatch }) => {
-    dispatch(updateLoading(true));
     const response = await confirmPasswordResetService(data);
-    dispatch(updateLoading(false));
     if (response.isError) toast.error(response.msg);
     else {
       toast.success(response.msg);
