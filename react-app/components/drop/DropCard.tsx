946dd33c13146bc4d7a1c82845a034839dc02dd3
AkashSDas <aakashdas368@gmail.com>
2022-02-08 09:23:26 +0530

    refactor drop card

--- react-app/components/drop/DropCard.tsx
+++ react-app/components/drop/DropCard.tsx
@@ -1,24 +1,15 @@
-import { useAppDispatch } from "lib/hooks/store";
+import { useAppDispatch, useAppSelector } from "lib/hooks/store";
 import { useRouter } from "next/router";
+import { selectDropById } from "store/drops/slice";
 import { reactOnDrop, unReactOnDrop, updateDropReaction } from "store/drops/thunk";
+import { IAuthor, IDrop, IReaction } from "store/drops/types";
 
-// import { IDrop } from "store/drops/slice";
-// import { reactOnDropThunk, toggleReactionOnDropThunk, unReactDropReactionThunk } from "store/drops/thunk";
 import ReactionButton from "./ReactionButton";
 
-const DropCard = ({
-  content,
-  user,
-  updatedAt,
-  reacted,
-  reactionsOnDrop,
-  id,
-  createdAt,
-}) => {
+const ProfilePic = ({ user }: { user: IAuthor }) => {
   const router = useRouter();
-  const dispatch = useAppDispatch();
 
-  const profilePic = () => (
+  return (
     <img
       className="h-[50px] w-[50px] rounded-full object-cover cursor-pointer"
       src={user.profilePic.URL}
@@ -29,76 +20,104 @@ const DropCard = ({
       }}
     />
   );
+};
 
-  const metadata = () => (
-    <div className="space-x-2">
-      <span className="font-bold text-text1">{user.username}</span>
-      <span>-</span>
-      <span className="font-bold">{updatedAt}</span>
-    </div>
+const Metadata = (metadata: { username: string; updatedAt: string }) => (
+  <div className="space-x-2">
+    <span className="font-bold text-text1">{metadata.username}</span>
+    <span>-</span>
+    <span className="font-bold">{metadata.updatedAt}</span>
+  </div>
+);
+
+const RedropButton = () => {
+  const style = `bg-card text-[13px] px-2 pt-[6px] pb-2 rounded-md`;
+  return (
+    <button type="button" className={style}>
+      💧 Redrop
+    </button>
+  );
+};
+
+interface IDropReactionButtonProps {
+  reaction: IReaction;
+  dropId: string;
+  reacted: { reaction: string; id: string } | null;
+}
+
+const DropReactionButton = (props: IDropReactionButtonProps) => {
+  const { reaction, reacted, dropId } = props;
+  const reactedStatus =
+    reacted && reacted?.reaction == reaction.name ? true : false;
+  const dispatch = useAppDispatch();
+
+  const unReact = () =>
+    dispatch(unReactOnDrop({ dropId, oldReaction: reaction.name }));
+
+  const changeReaction = () =>
+    dispatch(
+      updateDropReaction({
+        newReaction: reaction.name,
+        oldReaction: reacted.reaction,
+        dropId,
+      })
+    );
+
+  const react = () =>
+    dispatch(reactOnDrop({ dropId, newReaction: reaction.name }));
+
+  const handleClick = (e) => {
+    e.stopPropagation();
+    if (reacted) {
+      if (reacted.reaction === reaction.name) unReact();
+      else changeReaction();
+    } else react();
+  };
+
+  const { updatingReaction } = useAppSelector((state) =>
+    selectDropById(state.drops, dropId)
   );
 
   return (
-    <div
-      className="flex space-x-8 cursor-pointer"
-      onClick={() => router.push(`/drops/${id}`)}
-    >
-      {profilePic()}
+    <ReactionButton
+      dropId={dropId}
+      reaction={reaction.name}
+      emoji={reaction.emoji}
+      reacted={reactedStatus}
+      count={reaction.count}
+      onClick={handleClick}
+      disabled={updatingReaction}
+    />
+  );
+};
 
+const Reactions = ({ drop }: { drop: IDrop }) => (
+  <>
+    {drop.reactions.map((reaction, key) => (
+      <DropReactionButton
+        dropId={drop.id}
+        reacted={drop.reacted}
+        reaction={reaction}
+      />
+    ))}
+  </>
+);
+
+const DropCard = ({ drop }: { drop: IDrop }) => {
+  const router = useRouter();
+  const { user, updatedAt, content, id } = drop;
+
+  const navigateToDrop = () => router.push(`/drops/${id}`);
+
+  return (
+    <div className="flex space-x-8 cursor-pointer" onClick={navigateToDrop}>
+      <ProfilePic user={drop.user} />
       <div className="flex flex-col space-y-4 w-full">
-        {metadata()}
+        <Metadata username={user.username} updatedAt={updatedAt} />
         <p>{content}</p>
-
         <div className="space-x-4">
-          <button
-            type="button"
-            className={`bg-card text-[13px] px-2 pt-[6px] pb-2 rounded-md`}
-          >
-            💧 Redrop
-          </button>
-
-          <>
-            {reactionsOnDrop.map((reaction, key) => (
-              <ReactionButton
-                key={key}
-                dropId={id}
-                reaction={reaction.name}
-                emoji={reaction.emoji}
-                reacted={
-                  reacted && reacted?.reaction == reaction.name ? true : false
-                }
-                count={reaction.count}
-                onClick={(e) => {
-                  e.stopPropagation();
-
-                  // Check if drop is reacted by this user
-                  if (reacted) {
-                    if (reacted.reaction === reaction.name) {
-                      dispatch(
-                        unReactOnDrop({
-                          dropId: id,
-                          oldReaction: reaction.name,
-                        })
-                      );
-                    } else {
-                      dispatch(
-                        updateDropReaction({
-                          newReaction: reaction.name,
-                          oldReaction: reacted.reaction,
-                          dropId: id,
-                        })
-                      );
-                    }
-                  } else {
-                    // create new reaction and update state
-                    dispatch(
-                      reactOnDrop({ dropId: id, newReaction: reaction.name })
-                    );
-                  }
-                }}
-              />
-            ))}
-          </>
+          <RedropButton />
+          <Reactions drop={drop} />
         </div>
       </div>
     </div>
