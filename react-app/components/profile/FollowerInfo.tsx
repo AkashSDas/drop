6a74d8c046a59b1645dc9cd87f6185658d02a6eb
AkashSDas <aakashdas368@gmail.com>
2022-02-09 20:25:53 +0530

    refactor follower info

--- react-app/components/profile/FollowerInfo.tsx
+++ react-app/components/profile/FollowerInfo.tsx
@@ -1,39 +1,47 @@
-import { useAppSelector } from "lib/hooks/store";
-import { IFollower } from "store/profile/slice";
+import { IFollower } from "store/profile-followers/slice";
 
+import RollingAnimation from "@components/animation/RollingAnimation";
+import { ProfilePic } from "@components/drop/DropCard";
 import PrimaryButton from "@components/shared/PrimaryButton";
 
-const FollowerInfo = ({ info }: { info: IFollower }) => {
-  const { isFollowing, follower } = info;
-  const { togglingFollowStatus } = useAppSelector((state) => state.profile);
+interface Props {
+  follower: IFollower;
+}
 
-  const displayFollowBtn = () => {
-    if (isFollowing)
-      return (
-        <button className="text-text2 text-[17px] font-semibold pt-2 pb-[13px] px-[22px] rounded-lg hover:brightness-90 bg-card">
-          {togglingFollowStatus ? "Unfollowing..." : "Following"}
+const FollowButton = ({ follower }: Props) => {
+  const style =
+    "text-text2 text-[17px] font-semibold pt-2 pb-[13px] px-[22px] rounded-lg hover:brightness-90 bg-card";
+
+  const handleUnFollow = () => {};
+  const handleFollow = () => {};
+
+  if (follower.amIFollowing)
+    return (
+      <RollingAnimation>
+        <button className={style} onClick={handleUnFollow}>
+          {follower.isUpdatingFollowerStatus ? "Unfollowing..." : "Following"}
         </button>
-      );
-    else
-      return (
-        <PrimaryButton
-          text={togglingFollowStatus ? "Following..." : "Follow"}
-          onClick={async () => {}}
-        />
-      );
-  };
+      </RollingAnimation>
+    );
+
+  return (
+    <PrimaryButton
+      text={follower.isUpdatingFollowerStatus ? "Following..." : "Follow"}
+      onClick={handleFollow}
+    />
+  );
+};
+
+const FollowerInfo = ({ info }: { info: IFollower }) => {
+  const { follower } = info;
 
   return (
     <div className="flex justify-between items-center">
       <div className="space-x-8 flex items-center">
-        <img
-          className="h-[60px] w-[60px] rounded-full object-cover"
-          src={follower.profilePic.URL}
-          alt={follower.username}
-        />
+        <ProfilePic user={follower} />
         <div className="font-bold text-text1">{follower.username}</div>
       </div>
-      {displayFollowBtn()}
+      <FollowButton follower={info} />
     </div>
   );
 };
