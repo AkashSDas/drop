c384dc4a315978b5f1dae544e11febb43a41d218
AkashSDas <aakashdas368@gmail.com>
2022-02-09 19:33:43 +0530

    refactor profile drop card

--- react-app/components/profile/ProfileDropCard.tsx
+++ react-app/components/profile/ProfileDropCard.tsx
@@ -1,102 +1,83 @@
-import { useAppDispatch } from "lib/hooks/store";
+import { useAppDispatch, useAppSelector } from "lib/hooks/store";
 import { useRouter } from "next/router";
-import { IDrop } from "store/drops/slice";
-import { reactOnDropThunk, toggleReactionOnDropThunk, unReactDropReactionThunk } from "store/profile/thunk";
+import { IDrop } from "store/drops/types";
+import { selectProfileDropById } from "store/profile-drops/slice";
+import { reactOnDrop, unReactOnDrop, updateProfileDropReaction } from "store/profile-drops/thunk";
 
+import { IDropReactionButtonProps, Metadata, ProfilePic, RedropButton } from "@components/drop/DropCard";
 import ReactionButton from "@components/drop/ReactionButton";
 
-const ProfileDropCard = ({
-  content,
-  user,
-  updatedAt,
-  reacted,
-  reactionsOnDrop,
-  id,
-}: IDrop) => {
-  const router = useRouter();
+const DropReactionButton = (props: IDropReactionButtonProps) => {
+  const { reaction, reacted, dropId } = props;
+  const reactedStatus =
+    reacted && reacted?.reaction == reaction.name ? true : false;
   const dispatch = useAppDispatch();
 
-  const profilePic = () => (
-    <img
-      className="h-[50px] w-[50px] rounded-full object-cover cursor-pointer"
-      src={user.profilePic.URL}
-      alt={user.username}
-      onClick={(e) => {
-        e.stopPropagation();
-        router.push(`/profile/${user.id}/drops`);
-      }}
-    />
-  );
+  const unReact = () =>
+    dispatch(unReactOnDrop({ dropId, oldReaction: reaction.name }));
 
-  const metadata = () => (
-    <div className="space-x-2">
-      <span className="font-bold text-text1">{user.username}</span>
-      <span>-</span>
-      <span className="font-bold">{updatedAt}</span>
-    </div>
+  const changeReaction = () =>
+    dispatch(
+      updateProfileDropReaction({
+        newReaction: reaction.name,
+        oldReaction: reacted.reaction,
+        dropId,
+      })
+    );
+
+  const react = () =>
+    dispatch(reactOnDrop({ dropId, newReaction: reaction.name }));
+
+  const handleClick = (e) => {
+    e.stopPropagation();
+    if (reacted) {
+      if (reacted.reaction === reaction.name) unReact();
+      else changeReaction();
+    } else react();
+  };
+
+  const { updatingReaction } = useAppSelector((state) =>
+    selectProfileDropById(state.profileDrops, dropId)
   );
 
   return (
-    <div
-      className="flex space-x-8 cursor-pointer"
-      onClick={() => router.push(`/drops/${id}`)}
-    >
-      {profilePic()}
+    <ReactionButton
+      dropId={dropId}
+      reaction={reaction.name}
+      emoji={reaction.emoji}
+      reacted={reactedStatus}
+      count={reaction.count}
+      onClick={handleClick}
+      disabled={updatingReaction}
+    />
+  );
+};
 
+const Reactions = ({ drop }: { drop: IDrop }) => (
+  <>
+    {drop.reactions.map((reaction, key) => (
+      <DropReactionButton
+        dropId={drop.id}
+        reacted={drop.reacted}
+        reaction={reaction}
+      />
+    ))}
+  </>
+);
+
+const ProfileDropCard = ({ drop }: { drop: IDrop }) => {
+  const router = useRouter();
+  const navigateToDrop = () => router.push(`/drops/${drop.id}`);
+
+  return (
+    <div className="flex space-x-8 cursor-pointer" onClick={navigateToDrop}>
+      <ProfilePic user={drop.user} />
       <div className="flex flex-col space-y-4 w-full">
-        {metadata()}
-        <p>{content}</p>
-
+        <Metadata username={drop.user.username} updatedAt={drop.updatedAt} />
+        <p>{drop.content}</p>
         <div className="space-x-4">
-          <button
-            type="button"
-            className={`bg-card text-[13px] px-2 pt-[6px] pb-2 rounded-md`}
-          >
-            💧 Redrop
-          </button>
-
-          <>
-            {reactionsOnDrop.map((reaction, key) => (
-              <ReactionButton
-                key={key}
-                dropId={id}
-                reaction={reaction.name}
-                emoji={reaction.emoji}
-                reacted={
-                  reacted && reacted?.reaction == reaction.name ? true : false
-                }
-                count={reaction.count}
-                onClick={(e) => {
-                  e.stopPropagation();
-
-                  // Check if drop is reacted by this user
-                  if (reacted) {
-                    if (reacted.reaction === reaction.name) {
-                      dispatch(
-                        unReactDropReactionThunk({
-                          dropId: id,
-                          reaction: reaction.name,
-                        })
-                      );
-                    } else {
-                      dispatch(
-                        toggleReactionOnDropThunk({
-                          dropId: id,
-                          reaction: reaction.name,
-                          oldReaction: reacted.reaction,
-                        })
-                      );
-                    }
-                  } else {
-                    // create new reaction and update state
-                    dispatch(
-                      reactOnDropThunk({ dropId: id, reaction: reaction.name })
-                    );
-                  }
-                }}
-              />
-            ))}
-          </>
+          <RedropButton />
+          <Reactions drop={drop} />
         </div>
       </div>
     </div>
