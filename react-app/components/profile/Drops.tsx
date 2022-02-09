5c5dd240e84154cc3fbb6f734b8aef769177815f
AkashSDas <aakashdas368@gmail.com>
2022-02-09 19:33:54 +0530

    refactor profile drops

--- react-app/components/profile/Drops.tsx
+++ react-app/components/profile/Drops.tsx
@@ -1,7 +1,8 @@
 import { useAppDispatch, useAppSelector } from "lib/hooks/store";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { fetchUserDropsThunk, fetchUserMoreDropsThunk } from "store/profile/thunk";
+import { fetchInitialProfileDrops, fetchMoreProfileDrops } from "store/profile-drops/thunk";
 
 import DropsListViewLoading from "@components/drop/DropsListViewLoading";
 
@@ -10,47 +11,51 @@ import ProfileDropCard from "./ProfileDropCard";
 const Drops = () => {
   const dispatch = useAppDispatch();
   const token = useAppSelector((state) => state.user.token);
-  const { drops, dropsHasNext, loadingDrops, user } = useAppSelector(
-    (state) => state.profile
+  const { entities, isLoading, ids } = useAppSelector(
+    (state) => state.profileDrops
   );
+  const { user } = useAppSelector((state) => state.profile);
 
   useEffect(() => {
     if (user.id) {
-      dispatch(fetchUserDropsThunk({ init: true, userId: user.id }));
+      dispatch(fetchInitialProfileDrops({ limit: 10, userId: user.id }));
     }
   }, [token, user.id]);
 
-  return (
-    <div className="space-y-8">
-      {loadingDrops ? (
-        <DropsListViewLoading />
-      ) : (
-        <InfiniteScroll
-          dataLength={drops.length}
-          next={() => dispatch(fetchUserMoreDropsThunk(user.id))}
-          hasMore={dropsHasNext}
-          loader={<DropsListViewLoading />}
-          endMessage={<div className="font-semibold text-[23px]">The End</div>}
-          className="space-y-8"
-        >
-          {drops.map((d, key) => (
-            <div key={key} className="space-y-8">
-              <div className="border-b-[1px] border-solid border-[#32333B]"></div>
+  if (isLoading) return <DropsListViewLoading />;
 
-              <ProfileDropCard
-                content={d.content}
-                createdAt={d.createdAt}
-                id={d.id}
-                reacted={d.reacted}
-                reactionsOnDrop={d.reactionsOnDrop}
-                updatedAt={d.updatedAt}
-                user={d.user}
-              />
-            </div>
-          ))}
-        </InfiniteScroll>
-      )}
-    </div>
+  return (
+    <InfiniteScrollWrapper>
+      <>
+        {ids.map((id, key) => (
+          <div key={key} className="space-y-8">
+            <div className="border-b-[1px] border-solid border-[#32333B]"></div>
+            <ProfileDropCard drop={entities[id]} />
+          </div>
+        ))}
+      </>
+    </InfiniteScrollWrapper>
+  );
+};
+
+const InfiniteScrollWrapper = ({ children }: { children: JSX.Element }) => {
+  const dispatch = useAppDispatch();
+  const { hasNext, ids } = useAppSelector((state) => state.profileDrops);
+  const { user } = useAppSelector((state) => state.profile);
+
+  return (
+    <InfiniteScroll
+      dataLength={ids.length}
+      next={() =>
+        dispatch(fetchMoreProfileDrops({ limit: 10, userId: user.id }))
+      }
+      hasMore={hasNext}
+      loader={<DropsListViewLoading />}
+      endMessage={<div className="font-semibold text-[23px]">The End</div>}
+      className="space-y-8"
+    >
+      {children}
+    </InfiniteScroll>
   );
 };
 
